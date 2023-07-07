5fcea8cc73f959bb1ada8a407b1d5d2ce307694d
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-07 15:16:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Day 16 liczby całkowite(int) i zmiennoprzecinkowe(float)
+
+x = 4
+y = 10.5
+z = x + y # z = 14.5
+
+a = -7
+b = 3.14
+c = a * b # c = -22.18
+
+f = 100.0
+
+print(type(f))
+
+# Zadanie
+# Oblicz pole kwadratu o boku długości 5?
+
+a = 5
+b = (a) * (a) # b = 25
+
+
+# Oblicz obwód koła o promieniu 7
+
+a = 3.14
+b = 7
+c = 2 * (a) * (b) # c = 43.96
+
+
